28c8f04e862c9b5419c567ec28cecbcc9756ab9a
Sinoo <khoasinno@gmail.com>
2025-07-18 11:44:22 +0700

    update MODULE_ADDRESS

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AptosWalletAdapterProvider} from '@aptos-labs/wallet-adapter-react'
-
 import { Network } from "@aptos-labs/ts-sdk";
 
 createRoot(document.getElementById('root')).render(
